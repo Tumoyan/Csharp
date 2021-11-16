7e85c0b4cca1116974a3a98d8b5826228ccd2c16
Tumoyan <ntumo@bk.ru>
2021-11-16 16:24:49 +0300

    Новая ветка

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace Csharp
          System.Console.WriteLine("Введите радиус круга: " );
         double radius = Convert.ToDouble(Console.ReadLine());
         double area = Math.PI * Math.Pow(radius , 2);
+        System.Console.WriteLine("enter");
         System.Console.WriteLine("Площадь круга с радиусом {0} равана {1}" , radius, area);
 
 
