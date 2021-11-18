c4af40e4461321e6879cccf9ebbb1a472f3eceb6
Tumoyan <ntumo@bk.ru>
2021-11-18 18:36:18 +0300

    switch case

--- Program.cs
+++ Program.cs
@@ -9,8 +9,24 @@ namespace Csharp
         static void Main(string[] args)
         {
      
-         System.Console.WriteLine("Введите радиус круга: " );
+short user_input = Convert.ToInt16(Console.ReadLine());
 
+switch(user_input){
+    case 5: System.Console.WriteLine("Number is 5"); 
+    break;
+     case 20: System.Console.WriteLine("Number is 20"); 
+    break;
+     case 25: System.Console.WriteLine("Number is 25"); 
+    break;
+     case 155: System.Console.WriteLine("Number is 155"); 
+    break;
+
+    default: System.Console.WriteLine("Enter correct number such as 5,20,25,155");
+    break;
+
+
+
+}
 
         }
     }
