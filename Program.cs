5d146394ecd5610438c028c40ec53a19ff9c2e11
Tumoyan <ntumo@bk.ru>
2021-11-18 18:24:53 +0300

    new branch

--- Program.cs
+++ Program.cs
@@ -4,14 +4,12 @@ namespace Csharp
 {
     class Program
     {
+                //DEVELOP BRANCH
+
         static void Main(string[] args)
         {
      
          System.Console.WriteLine("Введите радиус круга: " );
-        double radius = Convert.ToDouble(Console.ReadLine());
-        double area = Math.PI * Math.Pow(radius , 2);
-        System.Console.WriteLine("enter");
-        System.Console.WriteLine("Площадь круга с радиусом {0} равана {1}" , radius, area);
 
 
         }
