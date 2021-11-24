587c6208186947b862507fe99f44b6158b80d846
Tumoyan <ntumo@bk.ru>
2021-11-24 10:34:38 +0300

    patch game

--- Program.cs
+++ Program.cs
@@ -14,17 +14,30 @@ int roll1 = dice.Next(1,7);
 int roll2 = dice.Next(1,7);
 int roll3 = dice.Next(1,7);
 
-int total = roll1 + roll2 + roll3;
+int result = roll1 + roll2 + roll3;
 
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}" );
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {result}");
 
-if(total > 14){
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    result +=2;
+}
+
+if ((roll1==roll2) && (roll2==roll3))
+{
+Console.WriteLine("You rolled triples! +6 bonus to total!");
+result +=6;
+}
+
+if ( result >= 15){
     Console.WriteLine("You win!");
 }
-if (total < 15){
+
+if (result < 15) {
     Console.WriteLine("Sorry, You lose.");
 }
-    }
+        }
      
 // Random dice = new Random();
 // int roll = dice.Next(1,7);
