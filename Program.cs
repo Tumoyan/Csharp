2a5c8fd8a170eb946b08bbd1c47114c6c5b75ead
Tumoyan <ntumo@bk.ru>
2021-11-24 11:52:18 +0300

    new task subscription

--- Program.cs
+++ Program.cs
@@ -7,37 +7,71 @@ namespace Csharp
                 //DEVELOP BRANCH
      static void Main(string[] args)
         {
+            Random random = new Random();
+int daysUntilExpiration = 1;//random.Next(12);
+int discountPercentage = 0;
 
-            Random dice = new Random();
-
-int roll1 = dice.Next(1,7);
-int roll2 = dice.Next(1,7);
-int roll3 = dice.Next(1,7);
-
-int result = roll1 + roll2 + roll3;
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {result}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+if (daysUntilExpiration == 0)
 {
-    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-    result +=2;
+    Console.WriteLine("Your subscription has expired.");
 }
-
-if ((roll1==roll2) && (roll2==roll3))
+else if (daysUntilExpiration == 1)
 {
-Console.WriteLine("You rolled triples! +6 bonus to total!");
-result +=6;
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon.  Renew now!");
 }
 
-if ( result >= 15){
-    Console.WriteLine("You win!");
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%.");
 }
 
-if (result < 15) {
-    Console.WriteLine("Sorry, You lose.");
-}
-        }
+            //Random dice = new Random();
+
+// // int roll1 = dice.Next(1,7);
+// // int roll2 = dice.Next(1,7);
+// // int roll3 = dice.Next(1,7);
+// int roll1 = 6;
+// int roll2 = 6;
+// int roll3 = 5;
+
+// int result = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {result}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2==roll3)) {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         result +=6;
+//     }
+//     else {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         result += 2;
+//     }
+// if (result >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (result >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (result ==7) {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else {
+//     Console.WriteLine("You win a kitten!");
+// }
      
 // Random dice = new Random();
 // int roll = dice.Next(1,7);
@@ -59,3 +93,4 @@ if (result < 15) {
 
 }
     }
+}
