ca3425d241da166b6adc2d10a062dfa256efbc88
Tumoyan <ntumo@bk.ru>
2021-11-24 09:57:37 +0300

    new game

--- Program.cs
+++ Program.cs
@@ -2,15 +2,33 @@
 
 namespace Csharp
 {
-    class Program
+    public class Program
     {
                 //DEVELOP BRANCH
      static void Main(string[] args)
         {
+
+            Random dice = new Random();
+
+int roll1 = dice.Next(1,7);
+int roll2 = dice.Next(1,7);
+int roll3 = dice.Next(1,7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}" );
+
+if(total > 14){
+    Console.WriteLine("You win!");
+}
+if (total < 15){
+    Console.WriteLine("Sorry, You lose.");
+}
+    }
      
-Random dice = new Random();
-int roll = dice.Next(1,7);
-Console.WriteLine(roll);
+// Random dice = new Random();
+// int roll = dice.Next(1,7);
+// Console.WriteLine(roll);
 
 // int fahrenheit = 94;
 // int celsNum = 32;
@@ -28,4 +46,3 @@ Console.WriteLine(roll);
 
 }
     }
-}
