c1dacaf3ad69dabc7ba9c987093399f22a734911
Tumoyan <ntumo@bk.ru>
2021-11-25 14:22:12 +0300

    new array task

--- Program.cs
+++ Program.cs
@@ -8,7 +8,15 @@ namespace Csharp
      static void Main(string[] args)
         {
             
+            string[] firstNnames = {"Bob" , "Narek" , "Ashot" , "Anna" , "Nur"};
 
+            foreach (string firstNname in firstNnames)
+            {
+                if (firstNname.StartsWith("N")) 
+                {
+                    System.Console.WriteLine(firstNname);
+                }
+            }
 }
-    }
+}
 }
