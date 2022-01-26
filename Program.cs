b98426a21a6c8af9870f5b69274ae68349dde4ba
Narek Tumoyan <80582150+Tumoyan@users.noreply.github.com>
2022-01-26 16:36:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -18,3 +18,70 @@ namespace Csharp
         }
     }
 }
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstNum, secondNum;
+                string action;
+
+                try
+                {
+                    Console.WriteLine("Enter firstNum");
+                    firstNum = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Enter secondNum");
+                    secondNum = double.Parse(Console.ReadLine());
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Input correct type");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Choose action: +, -, *, / ");
+                action = Console.ReadLine();
+
+                if (action == "+")
+                {
+                    Console.WriteLine(firstNum + secondNum);
+                }
+                else if (action == "-")
+                {
+                    Console.WriteLine(firstNum - secondNum);
+                }
+                else if (action == "*")
+                {
+                    Console.WriteLine(firstNum * secondNum);
+                }
+                else if (action == "/")
+                {
+                    if (secondNum == 0)
+                    {
+                        Console.WriteLine(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine(firstNum / secondNum);
+                    }
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
