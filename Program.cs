52d4ca4b10e69c91f53b5c3a7d0d6ab04a151421
Narek Tumoyan <80582150+Tumoyan@users.noreply.github.com>
2022-01-26 16:45:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -85,3 +85,15 @@ namespace MyProject
         }
     }
 }
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+ static void Main(string[] args)
+        {
+            int count = 0;
+            int limit = int.Parse(Console.ReadLine());
+            while (count < limit)
+            {
+                count++;
+                Console.WriteLine(count);
+            }
+        }
