f7a550cdcd48639b78530d5d6b73a6910940b75a
Narek Tumoyan <80582150+Tumoyan@users.noreply.github.com>
2022-01-27 14:16:36 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -195,3 +195,28 @@ using System;
                 }
                 Console.WriteLine();
             }
+/*
+  ******
+   *****
+    ****
+     ***
+      **
+       *
+ */
+            Console.WriteLine("Enter height triangle:");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = height; j >= i + 1; j--)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
