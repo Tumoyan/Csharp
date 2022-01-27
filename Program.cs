d427ca0828e6c3819a6ce1955d6c3525c5f98611
Narek Tumoyan <80582150+Tumoyan@users.noreply.github.com>
2022-01-27 13:41:42 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,15 @@
 ﻿using System;
-
 namespace Csharp
 {
     class Program
     {
-
         //MASTER BRANCH
         static void Main(string[] args)
         {
-     
          System.Console.WriteLine("Введите радиус круга: " );
         double radius = Convert.ToDouble(Console.ReadLine());
         double area = Math.PI * Math.Pow(radius , 2);
         System.Console.WriteLine("Площадь круга с радиусом {0} равана {1}" , radius, area);
-
-
         }
     }
 }
